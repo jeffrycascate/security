f6c3cb9c04b6d748cebb538acb41a42db8abe444
Asuras <jeffry.cascante@ucrso.info>
2019-01-05 11:26:56 -0600

    Updates

--- OS Agnostic/client.py
+++ OS Agnostic/client.py
@@ -2,50 +2,65 @@ import threading
 import time
 import os
 import sys
+import platform
 import fnmatch
 import psutil
-import platform
 import socket
 import json
 import requests
+import ftplib
 from datetime import datetime
+#import pymssql
+from getmac import get_mac_address
 
 # configuration services
 delimiter = '#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#'
 FolderNameProcess = "process"
 
 
-        
+class OS(object):
+    def __init__(self):
+        self.Name = ""
+        self.System = ""
+        self.Release = ""
+        self.Architecture = ""
 
-class ProcessRun(object):
+
+class Job(object):
     def __init__(self):
         self.Body = ""
         self.Path = ""
         self.NameMethod = ""
         self.Interval = 0
 
+
 class Host(object):
-    Items = []
+    Jobs = []
+    OS = OS()
+
     def __init__(self):
         self.HostName = ""
         self.IPLocal = ""
         self.IPPublic = ""
-        self.Items = []
+        self.MacAddress = ""
+        self.Jobs = []
+        self.OS = OS()
 
-class ThreadingExample(object):
+
+class ManagerThreadJob(object):
     """ Threading example class
     The run() method will be started and it will run in the background
     until the application exits.
     """
-    ProcessRun = object
+    Job = object
 
-    def __init__(self, ProcessRun):
+    def __init__(self, Job):
         """ Constructor
         :type interval: int
         :param interval: Check interval, in seconds
         """
-        self.interval = ProcessRun.Interval
-        self.ProcessRun = ProcessRun
+        self.interval = Job.Interval
+        self.Job = Job
 
         thread = threading.Thread(target=self.run, args=())
         thread.daemon = True                            # Daemonize thread
@@ -60,30 +75,30 @@ class ThreadingExample(object):
 
             try:
                 funcs = {}
-                exec(self.ProcessRun.Body, {}, funcs)
+                exec(self.Job.Body, {}, funcs)
                 for name in funcs:
-                    if name == self.ProcessRun.NameMethod:
-                        dd = funcs[name]
-                        varrr = dd()
+                    if name == self.Job.NameMethod:
+                        bodyFuntion = funcs[name]
+                        callResult = bodyFuntion()
 
             except Exception as e:
                 print("Ocurrio un error al ejecutar el archivo ",
-                      self.ProcessRun.Path, ", Original Exception: ", str(e))
+                      self.Job.Path, ", Original Exception: ", str(e))
                 print("End command that " +
                       datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
             time.sleep(self.interval)
 
 
 def ManagerPrecess():
-    ProcessRuns = []
+    Jobs = []
     listOfFiles = os.listdir(os.path.join(os.getcwd(), FolderNameProcess))
     pattern = "*.py"
     for entry in listOfFiles:
         if fnmatch.fnmatch(entry, pattern):
             print(entry)
-            item = ProcessRun()
+            item = Job()
             filePath = os.path.join(
-                os.getcwd(),FolderNameProcess,  entry)
+                os.getcwd(), FolderNameProcess,  entry)
 
             bodyRAW = open(filePath, "r").read()
             item.Body = bodyRAW.split(
@@ -93,37 +108,76 @@ def ManagerPrecess():
             item.NameMethod = configurationRAW[0]
             item.Interval = int(configurationRAW[1])
 
-
             item.Path = filePath
-            ProcessRuns.append(item)
-    return ProcessRuns
+            Jobs.append(item)
+    return Jobs
+
 
 def ManagerThreadProcess(process):
-    print('sdfa')
+    print('')
 
 def IpPublic():
-    url = 'http://api.ipstack.com/check?access_key=842a3f2afb39f85f84991e39c4033bf1&format=1'
-    headers = requests.utils.default_headers()  
-    headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-    ret = requests.get(url, headers=headers, verify=False)
-    body = json.loads(ret.content)
-    return body['ip']
+    result = "127.0.0.1"
+    try:
+        url = 'http://api.ipstack.com/check?access_key=842a3f2afb39f85f84991e39c4033bf1&format=1'
+        headers = requests.utils.default_headers()
+        headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+        ret = requests.get(url, headers=headers, verify=False)
+        body = json.loads(ret.content)
+        result = body['ip']
+    except Exception as e:
+        print("Ocurrio un error al tratar de extrar la ip local ",
+              ", Original Exception: ", str(e))
+    return result
+
+def IpLocal():
+    result = "127.0.0.1"
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        result = s.getsockname()[0]
+        s.close()
+    except Exception as e:
+        print("Ocurrio un error al tratar de extrar la ip local ",
+              ", Original Exception: ", str(e))
+    return result
+
+def MacAddress(ip):
+    result = ""
+    try:       
+        result =  get_mac_address(ip=ip)
+    except Exception as e:
+        print("Ocurrio un error al tratar de extrar la MacAddres ",
+              ", Original Exception: ", str(e))
+    return result
+
+
+def ManagerOS():
+    item = OS()
+    item.Name = os.name
+    item.System = platform.system()
+    item.Release = platform.release()
+    item.Architecture = platform.architecture()[0]
+    return item
+
 
 def ManagerHost():
     item = Host()
-    item.HostName =  platform.uname()[1]
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    item.IPLocal= s.getsockname()[0]
-    s.close()
+    item.HostName = platform.uname()[1]
+    item.IPLocal =  IpLocal()
     item.IPPublic = IpPublic()
+    item.MacAddress = MacAddress(item.IPLocal)
+    # OS
+    item.OS = ManagerOS()
+    item.Jobs = ManagerPrecess()
     return item
+
 
 if __name__ == "__main__":
     print('Starting process')
     ItemHost = ManagerHost()
     process = ManagerPrecess()
     for item in process:
-        example = ThreadingExample(item)
+        example = ManagerThreadJob(item)
     time.sleep(5000)
     print('End process')
