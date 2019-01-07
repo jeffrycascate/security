5ebb4acfd8fb4d21227c787ab70b37d1c6b12a59
Asuras <jeffry.cascante@ucrso.info>
2019-01-06 18:30:08 -0600

    Update

--- OS Agnostic/Server.py
+++ OS Agnostic/Server.py
@@ -3,19 +3,49 @@ import threading
 import time
 import os
 from datetime import datetime
+from ClientMySQL import *
+#endregion
+
+#region Configuration
+filePath = os.path.join(os.getcwd(), "Server.config")
+
+#MySQl Configuration
+MySQLHost = '186.177.106.36'
+MySQLUser = 'root'
+MySQLPassword = 'Jcv1821@t5'
+MySQLDatabase = 'osagnostic'
+
+#endregion
+
+#region Methods
+def  UpdateHosts():
+    db = CreateInstance(Host= MySQLHost, User= MySQLUser, Password= MySQLPassword, Database= MySQLDatabase)
+    query = "update host set State = b'0' where ADDTIME(updatedate , '0:00:10') <  SYSDATE();"
+    parameters = ()
+    ExisteInServer = ExecuteCommand(db, query, parameters)
+    db.close()
+    return ExisteInServer
 #endregion
 
 if __name__ == "__main__":
     IsRun = True
+    bodyRAW = open(filePath, "r").read()
+    
+    if bodyRAW == '0':
+        IsRun = False
+
     while IsRun:
-        filePath = os.path.join(os.getcwd(), "Server.config")
         bodyRAW = open(filePath, "r").read()
         if bodyRAW == '0':
             IsRun = False
             print("Stop services that " +
                       datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        time.sleep(20)
-        print("Executed to" +
+        else:
+            result = UpdateHosts()
+            print("Executed - Update Hosts " +
+                      datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        time.sleep(10)
+        print("Executed to " +
                       datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     print("Exit program to " +
                       datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
