3b69ac04a1073889c00d02125bb7b8a46f4bb0a9
Asuras <jeffry.cascante@ucrso.info>
2019-01-08 08:33:45 -0600

    Update job1.py
    Update

--- OSAgnostic/Configuration/job1.py
+++ OSAgnostic/Configuration/job1.py
@@ -1,8 +1,6 @@
 # 1;run;5;Job 1;Windows
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#
 # dasdfas Hola
-
-
 class Process(object):
     def __init__(self):
         self.Protocolo = 0
