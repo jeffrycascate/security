5027821cfd95da76d30b85c0a26bb14621ef4849
Asuras <jeffry.cascante@ucrso.info>
2019-01-11 16:19:41 -0600

    uPDATE
    uPDATE

--- OSAgnostic/Server/Naboo.Services/Startup.cs
+++ OSAgnostic/Server/Naboo.Services/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -26,7 +27,7 @@ namespace Naboo.Services
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           
+          
 
             services.AddCors();
             services.AddDbContext<OSAgnosticContext>();
@@ -45,7 +46,7 @@ namespace Naboo.Services
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            var cors = new EnableCorsAttribute("*", "*", "*");
             app.UseHttpsRedirection();
             app.UseMvc();
         }
